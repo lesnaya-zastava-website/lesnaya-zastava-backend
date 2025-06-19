a50c4bff30020d5e7fdd989a2e522a28971161e6
Zlis7 <denkalmy@gmail.com>
2025-06-19 18:08:12 +0000

    создание панели управления контента анонсов

---
+++ src/api/anonsy-i-aktualnye-shemy/services/anonsy-i-aktualnye-shemy.ts
@@ -0,0 +1,7 @@
+/**
+ * anonsy-i-aktualnye-shemy service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::anonsy-i-aktualnye-shemy.anonsy-i-aktualnye-shemy');
